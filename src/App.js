1d650e2809bc4eae201be40505a90d77837237b3
hssw24 <hssw@gmx.de>
2024-07-19 19:05:53 +0200

    111

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import React from "react";
 export default function HelloMessage() {
 
 
-return <h1>Hello, World</h1>; 
-return <h1>Hello, World</h1>;
-return <h1>Hello, World xxx</h1>;
+return <h1 align="center">Hello, World<br>Hello, World<br>Hello, World xxx</h1>;
 
 
 }
